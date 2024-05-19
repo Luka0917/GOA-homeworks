96dd4c70029f96435a0548dd543025f3397b6eba
Luka0917 <tatuashvililuka@gmail.com>
2024-05-19 21:21:31 +0400

    commit

---
+++ level3/level3.py
@@ -0,0 +1,11 @@
+#1
+print(8 + 2)
+print(8 - 2)
+print(8 * 2)
+print(8 / 2)
+#we want to make all 4 mathematical operations on numbers 8 and 2
+#2
+number= 8
+number1= 2
+print(number + number1)
+#we use this variables named number and number1 to group them together
