aae2137b9385c3d8a8a1a40bb3467e14ed70d020
Luka0917 <tatuashvililuka@gmail.com>
2024-05-28 21:19:42 +0400

    commit

---
+++ level6/homework.py
@@ -0,0 +1,9 @@
+# i am creating varible named "firstname" which outputs users name 
+firstname = input("what is your firstname?:")
+# i am creating varible named "lastname" which outputs users lastname
+lastname = input("what is your lastname?:")
+# i am creating varible named "age" which outputs users age
+age = input("how old are you?:")
+
+# i am combining all 3 varibles and printing them 
+print("you are " + firstname + " " + lastname + " you are " + age + " years old")
