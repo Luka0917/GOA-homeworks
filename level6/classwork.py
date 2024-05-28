364c6a8d6466b2499c9ff808f1c5ee0975fe2c1f
Luka0917 <tatuashvililuka@gmail.com>
2024-05-28 19:49:52 +0400

    commit

---
+++ level6/classwork.py
@@ -0,0 +1,11 @@
+number = input()
+number1 = input()
+
+print(int(number) + int(number1))
+print(int(number) - int(number1))
+print(int(number) * int(number1))
+print(int(number) / int(number1))
+
+print(int(number) == int(number1))
+print(int(number) > int(number1))
+print(int(number) < int(number1))
