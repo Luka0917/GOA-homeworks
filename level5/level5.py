04f9d6fcd8eeb653594e19e847d1c3f9f89bb158
Luka0917 <tatuashvililuka@gmail.com>
2024-05-28 12:40:29 +0400

    commit

---
+++ level5/level5.py
@@ -0,0 +1,62 @@
+#1
+
+# in 4 line, i am creating varible named "name" and storing string data "luka"
+name = "luka " 
+
+# in 7 line, i am creating varible named "surname" and storing string data "tatuashvili"
+surname = "tatuashvili" 
+
+# in 10 line, i am creating varible named "age" and storing integer data "15"
+age = 15 
+
+# in 13 line, i am creating varible named "height" and storing string data "170"
+height = 170 
+
+# in 16 line, i am combining this 4 varibles and printing a sentence with it
+print("hello my name is " + name + surname + " i am " + str(age) + " years old " + "i am " + str(height) + " cm tall")
+
+#2
+
+# in 21 line, i am creating varible named "number" and storing integer data "1"
+number = 1
+
+# in 24 line, i am creating varible named "number1" and storing integer data "2"
+number1 = 2
+
+# in 27 line, i am creating varible named "number2" and storing integer data "3"
+number2 = 3
+
+# in 30 line, i am creating varible named "number3" and storing integer data "4"
+number3 = 4
+
+# in 33 line, i am creating varible named "number4" and storing integer data "5"
+number4 = 5
+
+# in 36 line, i am creating varible named "number5" and storing integer data "6"
+number5 = 6
+
+# in 39 line, i am creating varible named "number6" and storing integer data "7"
+number6 = 7
+
+# in 42 line, i am creating varible named "number7" and storing integer data "8"
+number7 = 8
+
+# in 45 line, i am creating varible named "number8" and storing integer data "9"
+number8 = 9
+
+# in 48 line, i am creating varible named "number9" and storing integer data "0"
+number9 = 0
+
+# i am making mathematical operations on this 10 varibles 
+
+# in 53 line, i am making mathematical operations on varibles named "number" and "number5"
+print(number + number5)
+
+# in 56 line, i am making mathematical operations on varibles named "number6" and "number3"
+print(number6 - number3)
+
+# in 59 line, i am making mathematical operations on varibles named "number4" and "number9"
+print(number4 * number9)
+
+# in 62 line, i am making mathematical operations on varibles named "number7" and "number1"
+print(number7 / number1)
