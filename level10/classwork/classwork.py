631f973a2f118ba0d06dec70f9f622ebf4bc1d28
Luka0917 <tatuashvililuka@gmail.com>
2024-06-11 21:38:11 +0400

    commit

---
+++ level10/classwork/classwork.py
@@ -0,0 +1,2 @@
+for i in range(100):
+    print(str(i) + "." + "GOA is the best")
---
+++ level10/homework/homework.py
@@ -0,0 +1,58 @@
+print("-------1-------")
+
+for i in range(10):
+    print("i love TBILISI")
+
+print("-------2-------")
+
+for i in range(10):
+    print("i love studying in GOA")
+
+print("-------3-------")
+
+for i in range(10):
+    print(1+1)
+
+print("-------4-------")
+
+for i in range(10):
+    print("GOA lessons are fun")
+
+print("-------5-------")
+
+for i in range(10):
+    print(str(i) + "." + "i love GOA")
+
+print("-------6-------")
+
+name = "luka"
+for i in range(10):
+    print(name)
+
+print("-------7-------")
+
+name = input("what is your name?:")
+for i in range(10):
+    print(name)
+
+print("-------8-------")
+
+number = input("enter number:")
+for i in range(10):
+    print(int(number) + 1)
+
+print("-------9-------")
+
+number = 8
+print(number >= 1 and number <= 10)
+print(number >= 1 and number <= 4)
+print(number > 5 and number <= 10)
+print(number > 5 and number > 10)
+
+print("-------10-------")
+
+number = 5
+print(number >= 1 or number <= 10)
+print(number >= 1 or number <= 4)
+print(number > 5 or number <= 10)
+print(number > 5 or number > 10)
