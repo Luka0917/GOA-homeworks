71fc6d7e0b7321791c24bdbf9e2cbeb1b2221ad8
Luka0917 <tatuashvililuka@gmail.com>
2024-11-19 01:51:04 +0400

    commit

--- group 34/level 83/classwork/classwork.py
+++ group 34/level 83/classwork/classwork.py
@@ -1,8 +1,8 @@
 import random
 
-def password_function(length = 8, symbols = ["!", "@", "#", "$", "%", "&", "*"], numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]):
+def password_function(length = random.randint(4, 8), symbols = ["!", "@", "#", "$", "%", "&", "*"], numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]):
     password = ""
-    for i in range(4):
+    for i in range(int(length / 2)):
         password_symbols = random.choice(symbols)
         password_numbers = random.choice(numbers)
         password += password_symbols
