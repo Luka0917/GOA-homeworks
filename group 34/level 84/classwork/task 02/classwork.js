faf0649e7ee8b55df8616dd04b53c6bb95ce51bc
Luka0917 <tatuashvililuka@gmail.com>
2024-12-23 19:18:02 +0400

    commit

--- group 34/level 84/classwork/task 02/classwork.js
+++ group 34/level 84/classwork/task 02/classwork.js
@@ -1,13 +1,60 @@
 const body = document.querySelector("body");
 
-const pcName1 = document.createElement("h3");
-const info1 = document.createElement("p");
-const img1 = document.createElement("img");
+body.innerHTML = `
+    <div id="cart">
+        <p><i class="fa-solid fa-cart-shopping"></i> Cart</p>
+    </div>
+    <div id="count-div">
+        <p id="count">0</p>
+    </div>
+    <div id="main">
+        <div id="div-1" class="main-divs">
+            <p class="main-p">Shoe 1</p>
+            <img src="shoe1.jpg" alt="shoe 1">
+            <div class="icons">
+                <button class="icon-btn add"><i class="fa-solid fa-cart-shopping"></i> Add to cart</button>
+                <button class="icon-btn rem"><i class="fa-solid fa-trash"></i> Remove</button>
+            </div>
+        </div>
 
-const pc1 = {
-    name: pcName1.textContent = "pc 1",
-    info: info1.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing.",
-    img: img1.src = "https://www.punchtechnology.co.uk/wp-content/uploads/2024/02/vida2-1.jpg"
+        <div id="div-2" class="main-divs">
+            <p class="main-p">Shoe 2</p>
+            <img src="shoe2.jpg" alt="shoe 2">
+            <div class="icons">
+                <button class="icon-btn add"><i class="fa-solid fa-cart-shopping"></i> Add to cart</button>
+                <button class="icon-btn rem"><i class="fa-solid fa-trash"></i> Remove</button>
+            </div>
+        </div>
+
+        <div id="div-3" class="main-divs">
+            <p class="main-p">Shoe 3</p>
+            <img src="shoe3.jpg" alt="shoe 3">
+            <div class="icons">
+                <button class="icon-btn add"><i class="fa-solid fa-cart-shopping"></i> Add to cart</button>
+                <button class="icon-btn rem"><i class="fa-solid fa-trash"></i> Remove</button>
+            </div>
+        </div>
+    </div>
+`
+
+const add = document.getElementsByClassName("add");
+const rem = document.getElementsByClassName("rem");
+const countP = document.getElementById("count");
+
+let counter = 0;
+
+for(let i = 0; i < 3; i++){
+    add[i].addEventListener("click", function(){
+        counter++;
+        countP.textContent = counter;
+    })
+    rem[i].addEventListener("click", function(){
+        if(counter === 0){
+            alert("You can't remove!");
+        }
+        else{
+            counter--;
+            countP.textContent = counter;
+        }
+    })
 }
-
-body.appendChild(pc1);
