aa8d556a7e47798ff6a5617fc636c536a2b2cd8e
Luka0917 <tatuashvililuka@gmail.com>
2025-01-31 16:30:51 +0400

    commit

---
+++ group 34/level 113/classwork/task 02/classwork2.py
@@ -0,0 +1,11 @@
+# 8 kyu
+
+# https://www.codewars.com/kata/55225023e1be1ec8bc000390
+
+def greet(name):
+    return "Hello, my love!" if name == "Johnny" else f"Hello, {name}!"
+
+# https://www.codewars.com/kata/51c8991dee245d7ddf00000e
+
+def reverse_words(s):
+    return " ".join(s.split(" ")[::-1])
