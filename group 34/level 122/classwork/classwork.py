b000b9949cec370743d4f7009e1db661fb386756
Luka0917 <tatuashvililuka@gmail.com>
2025-02-18 00:54:24 +0400

    commit

---
+++ group 34/level 122/classwork/classwork.py
@@ -0,0 +1,36 @@
+# 8kyu
+
+# https://www.codewars.com/kata/56f6ad906b88de513f000d96
+
+# code
+
+# https://www.codewars.com/kata/56b1f01c247c01db92000076
+
+# code
+
+# 7kyu
+
+# https://www.codewars.com/kata/559590633066759614000063
+
+def min_max(lst):
+    min = lst[0]
+    max = lst[0]
+    for i in lst:
+        if i < min:
+            min = i
+        if i > max:
+            max = i
+    return [min, max]
+
+# https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9
+
+def number(lines):
+    result = [] 
+    for index in range(len(lines)):
+        word = str(index + 1) + ": " + lines[index]
+        result.append(word)
+    return result
+
+# https://www.codewars.com/kata/5412509bd436bd33920011bc
+
+# code
