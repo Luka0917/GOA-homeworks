6c9ea3984a13308f2930909e0ab3216fcf83d371
Luka0917 <tatuashvililuka@gmail.com>
2025-04-02 16:13:35 +0400

    commit

--- group 34/level 137/classwork/classwork.py
+++ group 34/level 137/classwork/classwork.py
@@ -1,3 +1,20 @@
 # 7kyu
 
 # https://www.codewars.com/kata/595519279be6c575b5000016
+
+def battle(group1, group2):
+    def func(group):
+        total_power = 0
+        for i in group:
+            total_power += ord(i) - ord('A') + 1
+        return total_power
+    
+    group1_power = func(group1)
+    group2_power = func(group2)
+    
+    if group1_power > group2_power:
+        return group1
+    elif group2_power > group1_power:
+        return group2
+    else:
+        return "Tie!"
