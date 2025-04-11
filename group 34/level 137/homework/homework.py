7a3c54ac5c138f4884eb23f84ea5f9847639ed2e
Luka0917 <tatuashvililuka@gmail.com>
2025-04-11 20:08:26 +0400

    commit

---
+++ group 34/level 137/homework/homework.py
@@ -0,0 +1,19 @@
+# ადვილი
+
+# 1. რა განსხვავებაა = და == ოპერატორებს შორის Python-ში?
+# 2. რას გამოიტანს len("Python")
+# 3. რა არის if statement და როგორ მუშაობს?
+
+# საშვალო
+
+# 1. რას გამოიტანს შემდეგი კოდი და რატომ?
+#   def greet(name="Guest"):
+#       return f"Hello, {name}!"
+
+#   print(greet())
+#   print(greet("Nino"))
+# 2. როგორ მუშაობს dictionary Python-ში?
+
+# რთული
+
+# 1. რა არის Python-ში lambda ფუნქცია?
