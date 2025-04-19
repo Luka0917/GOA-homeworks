2779dbb4f0c78b18ed2b9f72a25819ee27483f80
Luka0917 <tatuashvililuka@gmail.com>
2025-04-19 15:14:05 +0400

    commit

---
+++ group 34/level 143/homework/task 02-react/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <div className='flex justify-center items-center'>
+        <div className='flex justify-center items-center flex-col gap-[20px] border-2 rounded-[24px] mt-[20px] w-[350px] h-[200px]'>
+          <p className='text-[50px]'>{count}</p>
+          <div className='flex justify-center items-center gap-[20px]'>
+            <button onClick={() => setCount(count + 1)} className='border-2 rounded-[5px] py-[5px] px-[10px] bg-black text-white cursor-pointer hover:bg-white hover:text-black transition-bg duration-300'>Increase</button>
+            <button onClick={() => setCount(count - 1)} className='border-2 rounded-[5px] py-[5px] px-[10px] bg-black text-white cursor-pointer hover:bg-white hover:text-black transition-bg duration-300'>Decrease</button>
+            <button onClick={() => setCount(0)} className='border-2 rounded-[5px] py-[5px] px-[10px] bg-black text-white cursor-pointer hover:bg-white hover:text-black transition-bg duration-300'>Reset</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default App
