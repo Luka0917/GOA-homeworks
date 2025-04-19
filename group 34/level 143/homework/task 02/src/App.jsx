6438c6cbe4ada8846208f40afdd8ab8964c21de2
Luka0917 <tatuashvililuka@gmail.com>
2025-04-20 00:02:55 +0400

    commit

--- group 34/level 143/homework/task 02/src/App.jsx
+++ group 34/level 143/homework/task 02/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState('No')
   return (
     <>
-      <div className='flex justify-center items-center'>
+      <div className='flex justify-center items-center flex-col gap-[20px]'>
         <div className='flex justify-center items-center flex-col gap-[20px] border-2 rounded-[24px] mt-[20px] w-[350px] h-[200px]'>
           <p className='text-[50px]'>{count}</p>
           <div className='flex justify-center items-center gap-[20px]'>
@@ -12,6 +13,10 @@ function App() {
             <button onClick={() => setCount(count - 1)} className='border-2 rounded-[5px] py-[5px] px-[10px] bg-black text-white cursor-pointer hover:bg-white hover:text-black transition-bg duration-300'>Decrease</button>
             <button onClick={() => setCount(0)} className='border-2 rounded-[5px] py-[5px] px-[10px] bg-black text-white cursor-pointer hover:bg-white hover:text-black transition-bg duration-300'>Reset</button>
           </div>
+        </div>
+        <div className='flex justify-center items-center flex-col gap-[20px] border-2 rounded-[24px] w-[350px] h-[200px]'>
+          <p className='text-[20px]'><b>Is programmer:</b> {text}</p>
+          <button onClick={() => setText(text === 'Yes' ? 'No' : 'Yes')} className='border-2 rounded-[5px] py-[5px] px-[10px] bg-black text-white cursor-pointer hover:bg-white hover:text-black transition-bg duration-300'>Change Text</button>
         </div>
       </div>
     </>
