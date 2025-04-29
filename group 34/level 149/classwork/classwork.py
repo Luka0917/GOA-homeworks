2ac33ec0771aabcbc6c2906fc174f566169abbf5
Luka0917 <tatuashvililuka@gmail.com>
2025-04-29 23:11:10 +0400

    commit

--- group 34/level 149/classwork/classwork.py
+++ group 34/level 149/classwork/classwork.py
@@ -9,3 +9,21 @@ def cap_me(arr):
 
 def arr_check(arr):
     return all(type(i) == list for i in arr)
+
+# https://www.codewars.com/kata/56a921fa8c5167d8e7000053
+
+def password(st):
+    have_upper = False
+    have_lower = False
+    have_num = False
+    for i in st:
+        if len(st) >= 8:
+            if i.isalpha() and i.isupper():
+                have_upper = True
+            if i.isalpha() and i.islower():
+                have_lower = True 
+            if i.isdigit():
+                have_num = True
+        else:
+            return False
+    return have_upper and have_lower and have_num
