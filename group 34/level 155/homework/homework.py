1d3ccb7d2783b9f752f5ce668b7304e6439ed326
Luka0917 <tatuashvililuka@gmail.com>
2025-05-14 17:01:46 +0400

    commit

--- group 34/level 155/homework/homework.py
+++ group 34/level 155/homework/homework.py
@@ -13,3 +13,38 @@ def solve(a, b):
             re_b.append(i)
     result = re_a + re_b
     return ''.join(result)
+
+# https://www.codewars.com/kata/5a2d70a6f28b821ab4000004/train/python
+
+def largest_sum(s):
+    s = s.split('0')
+    max_sum = 0
+    for i in s:
+        digit_sum = sum(int(x) for x in i)
+        if digit_sum > max_sum:
+            max_sum = digit_sum
+    return max_sum
+
+# https://www.codewars.com/kata/590f5b4a7bbb3e246000007d/train/python
+
+def comes_after(st, l):
+    l = l.lower()
+    result = ""
+    for i in range(len(st) - 1):
+        current_char = st[i]
+        next_char = st[i + 1]
+        if current_char.lower() == l and next_char.isalpha():
+            result += next_char
+    return result
+
+# https://www.codewars.com/kata/565b9d6f8139573819000056/train/python
+
+def decode(message):
+    result = ""
+    for i in message:
+        if i.isalpha():
+            decoded_char = chr(ord('a') + (25 - (ord(i) - ord('a'))))
+            result += decoded_char
+        else:
+            result += i
+    return result
