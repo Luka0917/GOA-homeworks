0480c2886ebcacb9286c6e1f1b8fbd7dfdd86782
Luka0917 <tatuashvililuka@gmail.com>
2025-05-15 22:35:13 +0400

    commit

--- group 34/level 158/classwork/classwork.py
+++ group 34/level 158/classwork/classwork.py
@@ -2,3 +2,16 @@
 
 # https://www.codewars.com/kata/585d7d5adb20cf33cb000235
 
+def find_uniq(arr):
+    first = arr[0]
+    second = arr[1]
+    third = arr[2]
+    if first == second:
+        common = first
+    elif first == third:
+        common = first
+    else:
+        common = second
+    for i in arr:
+        if i != common:
+            return i
