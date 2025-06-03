63107bc6c623265d1da214e9c1cff905c5e4b181
Luka0917 <tatuashvililuka@gmail.com>
2025-06-03 21:35:48 +0400

    commit

---
+++ group 34/level 166/classwork/classwork.js
@@ -0,0 +1,8 @@
+// 8kyu
+
+// https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+
+function switchItUp(number){
+  let words = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+  return words[number]
+}
