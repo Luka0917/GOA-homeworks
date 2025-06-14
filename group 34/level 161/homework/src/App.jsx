2b0e5f2fd1b8bf7810f910195ba5a2843565f452
Luka0917 <tatuashvililuka@gmail.com>
2025-06-14 14:50:04 +0400

    commit

--- group 34/level 161/homework/src/App.jsx
+++ group 34/level 161/homework/src/App.jsx
@@ -3,6 +3,9 @@ import Home from './components/Home'
 function App() {
   return (
     <>
+      <div className='flex justify-center items-center flex-col h-screen bg-[#FBFBFB]'>
+        <Home />
+      </div>
     </>
   )
 }
