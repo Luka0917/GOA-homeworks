8c5e8bead147ba04c0afc5e3bf0a9ae9ce44d568
Luka0917 <tatuashvililuka@gmail.com>
2025-12-22 16:10:24 +0400

    commit

--- group 34/level 226 🟥/classwork/src/App.jsx
+++ group 34/level 226 🟥/classwork/src/App.jsx
@@ -8,40 +8,36 @@ function App() {
     const formData = new FormData(e.target);
     const x = Number(formData.get('x'));
     const y = Number(formData.get('y'));
-    console.log(x);
-    console.log(y);
     const newDivs = [];
-    for (let row = 0; row < x; row++) {
+    for(let row = 0; row < x; row++){
       const rowDivs = [];
-      for (let col = 0; col < y; col++) {
-        rowDivs.push(<div key={`${row}-${col}`} className="border w-[30px] h-[30px]"></div>);
+      for(let col = 0; col < y; col++){
+        rowDivs.push(<div key={`${row}-${col}`} className="border w-[50px] h-[50px]"></div>);
       }
       newDivs.push(rowDivs);
     }
-
     setDivs(newDivs);
-    console.log(newDivs);
   }
 
   return (
     <>
-      <div>
-        <form onSubmit={handleForm}>
+      <div className="flex justify-center items-center flex-col pt-[50px]">
+        <form onSubmit={handleForm} className="flex justify-center items-center flex-col gap-[5px]">
           <div>
-            <label htmlFor="">Grid X:</label>
-            <input type="number" min={1} name="x" required className="border" />
+            <label htmlFor="x" className="text-[20px] font-medium">Grid X:</label>
+            <input type="number" min={1} name="x" id="x" required className="border rounded-[7px] px-[5px] py-0.5 ml-[7px]" />
           </div>
           <div>
-            <label htmlFor="">Grid Y:</label>
-            <input type="number" min={1} name="y" required className="border" />
+            <label htmlFor="y" className="text-[20px] font-medium">Grid Y:</label>
+            <input type="number" min={1} name="y" id="y" required className="border rounded-[7px] px-[5px] py-0.5 ml-[7px]" />
           </div>
-          <button>Submit</button>
+          <button className="border border-black rounded-xl px-[5px] py-1.5 w-[200px] bg-black text-white cursor-pointer hover:bg-white hover:text-black transition duration-200">Submit</button>
         </form>
 
-        <div className="border border-purple-500 p-5">
+        <div className="mt-[100px]">
           {divs.map((el, i) => (
             <div key={i} className="flex">
-              {el.map((box) => box)}
+              {el.map(cube => cube)}
             </div>
           ))}
         </div>
