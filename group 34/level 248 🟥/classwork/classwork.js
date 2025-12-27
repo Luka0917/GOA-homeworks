21b8bc070770090c8059f8ab03186ba66bcf8acb
Luka0917 <tatuashvililuka@gmail.com>
2025-12-27 22:53:11 +0400

    commit

--- group 34/level 248 🟥/classwork/classwork.js
+++ group 34/level 248 🟥/classwork/classwork.js
@@ -1,15 +1,11 @@
-//! singly linked list
-
-//^ pushBack, pushFront, popBack, popFront, get(index), insert(index, value), erase(index), find(value), clear(), reverse(), toArray()
-
-
 class Node{
     constructor(value){
         this.value = value;
         this.next = null;
+        this.prev = null;
     }
 }
-class LinkdeList {
+class DoubleLinkdeList {
     constructor(){
         this.head = null;
         this.size = 0;
@@ -19,27 +15,29 @@ class LinkdeList {
     //* O(1)
     pushBack(value){
         const newNode = new Node(value);
-
         if(!this.head){
             this.head = newNode;
             this.tail = newNode;
         }else{
+            newNode.prev = this.tail;
             this.tail.next = newNode;
             this.tail = newNode;
         }
-        this.size++;
+        this.size++;    
     }
 
     //* O(1)
     pushFront(value){
         const newNode = new Node(value);
-        newNode.next = this.head;
-        this.head = newNode;
-
-        if(!this.tail){
+        if(!this.head){
+            this.head = newNode;
             this.tail = newNode;
+        }else{
+            newNode.next = this.head;
+            this.head.prev = newNode;
+            this.head = newNode;
         }
-        this.size++;
+        this.size++;    
     }
 
     //* O(n)
@@ -48,14 +46,10 @@ class LinkdeList {
         if(this.head == this.tail){
             this.head = null;
             this.tail = null;
+        }else{
+            this.tail = this.tail.prev;
+            this.tail.next = null;
         }
-        let current = this.head;
-
-        while(current.next != this.tail){
-            current = current.next;
-        }
-        current.next = null;
-        this.tail = current;
         this.size--;
     }
 
@@ -89,14 +83,11 @@ class LinkdeList {
         if(index === this.size) this.pushBack(value);
 
         const newNode = new Node(value);
-        let current = this.head;
-        let i = 0;
-        while(i < index-1){
-            current = current.next;
-            i++;
-        }
-        newNode.next = current.next;
-        current.next = newNode;
+        let current = this.get(index);
+        newNode.prev = current.prev;
+        newNode.next = current;
+        current.prev.next = newNode;
+        current.prev = newNode;
         this.size++;
     }
 
@@ -105,15 +96,12 @@ class LinkdeList {
         if(index < 0 || index >= this.size) return;
 
         if(index === 0) this.popFront();
+        if(index === this.size - 1) this.popBack();
 
-        let current = this.head;
-        let i = 0;
-        while(i < index-1){
-            current = current.next;
-            i++
-        }
-        current.next = current.next.next;
-        if(index === this.size - 1) this.tail = current;
+        const node = this.get(index);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+
         this.size--;
     }
 
@@ -161,40 +149,3 @@ class LinkdeList {
         return result;
     }
 };
-
-
-// 83. Remove Duplicates from Sorted List
-// https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
-
-var deleteDuplicates = function(head){
-    let current = head;
-    while(current && current.next){
-        if(current.val === current.next.val) current.next = current.next.next;
-        else current = current.next;
-    }
-    return head
-};
-
-
-function deleteDuplicates2(list){
-    if(!list.head) return;
-    let current = list.head;
-    let i = 0;
-    while(current && current.next){
-        if(current.value === current.next.value) list.erase(i + 1);
-        else{
-            current = current.next;
-            i++;
-        }
-    }
-    return list.head;
-}
-
-let list = new LinkdeList();
-list.pushBack(1);
-list.pushBack(1);
-list.pushBack(2);
-list.pushBack(3);
-list.pushBack(3);
-deleteDuplicates2(list);
-console.log(list.toArray());
